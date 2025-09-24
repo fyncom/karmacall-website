1b3417d5e2944b71a1467d73304f626428c5bb0d
Adrian E. Garcia <52181546+adrianegraphene@users.noreply.github.com>
2025-09-24 06:39:31 -0700

    fixing the dark mode views

--- src/pages/ai-collaboration.js
+++ src/pages/ai-collaboration.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
-import { FaRobot, FaLightbulb, FaChartLine, FaUsers, FaCog, FaRocket } from "react-icons/fa"
+import { FaLightbulb, FaChartLine, FaUsers, FaCog, FaRocket } from "react-icons/fa"
 import Seo from "../components/seo"
 import "../components/ai-collaboration.css"
 
