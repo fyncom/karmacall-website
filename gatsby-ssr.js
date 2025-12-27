61457a5bd3b9d510d5c20a5d3ab6c5b2a104c892
Adrian E. Garcia <52181546+adrianegraphene@users.noreply.github.com>
2025-12-27 01:56:02 -0800

    trying fix for favicon display in search engines

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -16,6 +16,23 @@ exports.wrapRootElement = ({ element }) => {
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: `en` })
+  setHeadComponents([
+    <link key="favicon-ico" rel="icon" href="/favicon.ico" sizes="any" />,
+    <link
+      key="favicon-48"
+      rel="icon"
+      type="image/png"
+      sizes="48x48"
+      href="/icons/icon-48x48.png"
+    />,
+    <link
+      key="favicon-32"
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/favicon-32x32.png"
+    />,
+  ])
 }
